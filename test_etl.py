af6682b2012ae741ce5844972298a633882c1b52
khanin97 <khanin.cht@gmail.com>
2025-06-13 13:53:42 +0700

    test2

--- test_etl.py
+++ test_etl.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from datetime import datetime
+import pytz
 
 dfInput = pd.read_csv('sample_products.csv')
 dfOutput = pd.read_csv('output/clean_products.csv')
 
+bangkok = pytz.timezone("Asia/Bangkok")
+now = datetime.now(bangkok)
+
+
 with open('output/test_result.txt', 'a') as f:
-    f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
+    f.write(datetime.now(bangkok).strftime("%d-%m-%Y %H:%M:%S") + '\n')
 
     if dfOutput.dtypes['unit_price'] == 'int64':
         f.write("Case 1: Pass\n")
